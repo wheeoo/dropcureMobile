77bfb7a7d90b11854951091f23a6b58d8e34a538
shiv2299 <shiv2299@gmail.com>
2021-03-17 20:15:41 +0530

    app name change

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Dropcure',
+      title: 'Dropcure Driver',
       theme: ThemeData(primarySwatch: Colors.blue, fontFamily: "Roboto"),
       home: SplashScreen(),
       routes: {
